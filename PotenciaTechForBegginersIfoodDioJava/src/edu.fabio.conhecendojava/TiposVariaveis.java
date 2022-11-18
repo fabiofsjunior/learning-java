fcb9eab8aad9d81f72424a3c32da744a9be68a07
Fábio F S Jr <59260204+fabiofsjunior@users.noreply.github.com>
2022-11-17 22:10:13 -0300

    update operadores em java

---
+++ PotenciaTechForBegginersIfoodDioJava/src/edu.fabio.conhecendojava/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+/*int
+ * long = 10.1040L;
+ * double = 2500;
+ * float = 3.14F;
+ * bolean = true or false;
+ * String = "uma string qualquer";
+ * char = 'M';
+ */
+
+ /*final NOME_EM_MAIUSCULO = ATRIBUIÇÃO; (Isto é uma constante)*/
