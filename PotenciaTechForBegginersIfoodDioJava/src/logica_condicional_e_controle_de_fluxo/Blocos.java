300981f6ed0da0af6a5ae34c1b050e5c1fd6bd91
Fábio F S Jr <59260204+fabiofsjunior@users.noreply.github.com>
2022-11-24 04:22:04 -0300

    Update - Curso jDevTrain, added folders to github

--- PotenciaTechForBegginersIfoodDioJava/src/logica_condicional_e_controle_de_fluxo/Blocos.java
+++ PotenciaTechForBegginersIfoodDioJava/src/logica_condicional_e_controle_de_fluxo/Blocos.java
@@ -3,13 +3,7 @@ package logica_condicional_e_controle_de_fluxo;
 public class Blocos {
     public static void main(String[] args) {
         
-    
-    if (!b2)
-    System.out.println("Verdade");
-    else {
-    System.out.println("Mentira");
-    System.out.println("Mentira");
-    }
+
     /*
      * #BLOCOS
      * Locais
@@ -18,5 +12,6 @@ public class Blocos {
         * Estáticos.
         * Instância.
      */
+    }
 }
 
