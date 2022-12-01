0a9ea5d3bf6959f3e53fdd5b680aecb6f1e8d103
Fábio F S Jr <59260204+fabiofsjunior@users.noreply.github.com>
2022-11-30 23:14:41 -0300

    update - ajustes nos pacotes e pastas

---
+++ JavaDevFullStack/src/cursojava.exercicios/Calculando.java
@@ -0,0 +1,46 @@
+import javax.swing.JOptionPane;
+
+public class Calculando {
+
+    public static void main(String[] args) {
+
+        double nota1 = 12;
+        double nota2 = 20;
+        double nota3 = 30;
+        double nota4 = 40;
+
+        System.out.println("Média das notas = " + (nota1 + nota2 + nota3 + nota4) / 4);
+
+        double mediaFinal = ((nota1 + nota2 + nota3 + nota4) / 4);
+
+        if (mediaFinal > 20) {
+            System.out.println("Está aprovado com a média = " + mediaFinal + "!");
+        }
+
+        String carros = JOptionPane.showInputDialog("Informe a quantidade de carros:");
+        String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas:");
+
+        double carrosNumero = Double.parseDouble(carros);
+        double pessoasNumero = Double.parseDouble(pessoas);
+
+        int divisao = (int) (carrosNumero / pessoasNumero);
+        double resto = carrosNumero % pessoasNumero;
+
+        System.out.println(carros);
+        System.out.println(pessoas);
+        System.out.println(divisao);
+        System.out.println(resto);
+
+
+        int resposta = JOptionPane.showConfirmDialog(null,"Deseja ver o resultado da divisão?");
+
+        if (resposta == 0){
+            JOptionPane.showMessageDialog(null, "Divisão para cada pessoa deu: "+ divisao + ".");
+        }
+
+        System.out.println(resposta);
+
+        JOptionPane.showMessageDialog(null,
+                "Cada pessoa ficou com: " + divisao + ", e sobraram: " + resto + " carros.");
+    }
+}
