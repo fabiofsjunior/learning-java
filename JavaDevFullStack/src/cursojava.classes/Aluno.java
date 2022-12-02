e41acff5d377ba3bc1f2c48cd5f7558300010785
Fábio F S Jr <59260204+fabiofsjunior@users.noreply.github.com>
2022-12-02 00:20:40 -0300

    new - interação com o usuário poo

--- JavaDevFullStack/src/cursojava.classes/Aluno.java
+++ JavaDevFullStack/src/cursojava.classes/Aluno.java
@@ -1,6 +1,6 @@
 public class Aluno {
 
-    //Atributos
+    // Atributos
     String nome;
     int idade;
     String dataNascimento;
@@ -11,23 +11,145 @@ public class Aluno {
     String nomeEscola;
     String serieMatriculado;
 
+    double nota1;
+    double nota2;
+    double nota3;
+    double nota4;
 
-    //Construtores
-    public Aluno(){
+    // Construtores
+    public Aluno() {
 
     }
 
-    public Aluno(String nomePadrao){
+    public Aluno(String nomePadrao) {
         nome = nomePadrao;
     }
 
-    public Aluno(String nomePadrao, int idadePadrao){
+    public Aluno(String nomePadrao, int idadePadrao) {
         nome = nomePadrao;
         idade = idadePadrao;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-    
+    public double getNota1() {
+        return nota1;
+    }
 
-    
+    public double getNota2() {
+        return nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public double getNota4() {
+        return nota4;
+    }
+
+    // SETTERS -----------------------------------
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setRegistroGeral(String registroGeral) {
+        this.registroGeral = registroGeral;
+    }
+
+    public void setNumeroCpf(String numeroCpf) {
+        this.numeroCpf = numeroCpf;
+    }
+
+    public void setNomeMae(String nomeMae) {
+        this.nomeMae = nomeMae;
+    }
+
+    public void setDataMatricula(String dataMatricula) {
+        this.dataMatricula = dataMatricula;
+    }
+
+    public void setNomeEscola(String nomeEscola) {
+        this.nomeEscola = nomeEscola;
+    }
+
+    public void setSerieMatriculado(String serieMatriculado) {
+        this.serieMatriculado = serieMatriculado;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+
+    public void setNota4(double nota4) {
+        this.nota4 = nota4;
+    }
+
+    // GETTERS -----------------------------------
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getRegistroGeral() {
+        return registroGeral;
+    }
+
+    public String getNumeroCpf() {
+        return numeroCpf;
+    }
+
+    public String getNomeMae() {
+        return nomeMae;
+    }
+
+    public String getDataMatricula() {
+        return dataMatricula;
+    }
+
+    public String getNomeEscola() {
+        return nomeEscola;
+    }
+
+    public String getSerieMatriculado() {
+        return serieMatriculado;
+    }
+
+    /* Calcula a média do aluno. */
+    public double getMediaNota() {
+        return (nota1 + nota2 + nota3 + nota4) / 4;
+    }
+
+    public boolean getAlunoAprovado(){
+        double media = this.getMediaNota();
+        if (media >= 7){
+            return true;
+        }else{
+            return false;
+        }
+    }
+   
+
 }
--- JavaDevFullStack/src/cursojava.executavel/PrimeiraClasse.java
+++ JavaDevFullStack/src/cursojava.executavel/PrimeiraClasse.java
@@ -1,16 +1,52 @@
+import javax.swing.JOptionPane;
+
 public class PrimeiraClasse {
     public static void main(String[] args) {
-        
+
+        String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
+        String idade = JOptionPane.showInputDialog("Qual a idade?");
+        String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento?");
+        String registroGeral = JOptionPane.showInputDialog("Qual nr do Registro Geral?");
+        String numeroCpf = JOptionPane.showInputDialog("Qual o CPF?");
+        String nomeMae = JOptionPane.showInputDialog("Qual o nome da 'MÃE' do aluno?");
+        String dataMatricula = JOptionPane.showInputDialog("Qual o nr da matricula?");
+        String nomeEscola = JOptionPane.showInputDialog("Qual o nome da escola?");
+        String serieMatriculado = JOptionPane.showInputDialog("Qual a série matriculada?");
+    
+        String nota1 = JOptionPane.showInputDialog("Digite a 1ª nota: ");
+        String nota2 = JOptionPane.showInputDialog("Digite a 2ª nota: ");
+        String nota3 = JOptionPane.showInputDialog("Digite a 3ª nota: ");
+        String nota4 = JOptionPane.showInputDialog("Digite a 4ª nota: ");
+
+
         // Agora temos um objeto real namemória
-        //Instâncias
-        //Aluno1 é referência ao Objeto Aluno
-        Aluno aluno1 = new Aluno(); //João
-        Aluno aluno2 = new Aluno(); //Pedro
-        Aluno aluno3 = new Aluno(); //Vitor
+        // Instâncias
+        // Aluno1 é referência ao Objeto Aluno
+        Aluno aluno1 = new Aluno(); // João
 
-       Aluno aluno4 = new Aluno("Maria");
+        aluno1.setNome(nome);
+        aluno1.setIdade(Integer.valueOf(idade));
+        aluno1.setDataNascimento(dataNascimento);
+        aluno1.setRegistroGeral(registroGeral);
+        aluno1.setNumeroCpf(numeroCpf);
+        aluno1.setNomeMae(nomeMae);
+        aluno1.setDataMatricula(dataMatricula);
+        aluno1.setNomeEscola(nomeEscola);
+        aluno1.setSerieMatriculado(serieMatriculado);
+        aluno1.setNota1(Double.parseDouble(nota1));
+        aluno1.setNota2(Double.parseDouble(nota2));
+        aluno1.setNota3(Double.parseDouble(nota3));
+        aluno1.setNota4(Double.parseDouble(nota4));
+        
 
-       Aluno aluno5 = new Aluno("Zé", 50);
+
+        System.out.println("Nome: " + aluno1.getNome());
+        System.out.println("Idade: " + aluno1.getIdade());
+        System.out.println("Data de nascimento: " + aluno1.getDataNascimento());
+        System.out.println("Nome da mãe : " + aluno1.getNomeMae());
+        System.out.println("Média da nota: " + aluno1.getMediaNota());
+        System.out.println("Resultado: " + (aluno1.getAlunoAprovado() ? "Aprovado!" : "Reprovado!"));
+
 
     }
 }
