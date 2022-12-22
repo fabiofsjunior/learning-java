efeefe42ad40e3525bbb7592ffa5facfd9227ffc
Fábio F S Jr <59260204+fabiofsjunior@users.noreply.github.com>
2022-12-22 16:56:50 -0300

    new - introdução ao conceito prático de herança.

--- JavaDevFullStack/src/cursojava.classes/Aluno.java
+++ JavaDevFullStack/src/cursojava.classes/Aluno.java
@@ -1,111 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Aluno {
+public class Aluno extends Pessoa {
 
-    // Atributos
-    String nome;
-    int idade;
-    String dataNascimento;
-    String registroGeral;
-    String numeroCpf;
-    String nomeMae;
-    String dataMatricula;
-    String nomeEscola;
-    String serieMatriculado;
-
-    List <Disciplina> disciplinas = new ArrayList<Disciplina>();
+    List<Disciplina> disciplinas = new ArrayList<Disciplina>();
 
     public void setDisciplinas(List<Disciplina> disciplinas) {
         this.disciplinas = disciplinas;
     }
+
     public List<Disciplina> getDisciplinas() {
         return disciplinas;
     }
 
-  
-    // Construtor -----------------------------------
     public Aluno() {
 
     }
-
-    // SETTERS -----------------------------------
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public void setDataNascimento(String dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public void setRegistroGeral(String registroGeral) {
-        this.registroGeral = registroGeral;
-    }
-
-    public void setNumeroCpf(String numeroCpf) {
-        this.numeroCpf = numeroCpf;
-    }
-
-    public void setNomeMae(String nomeMae) {
-        this.nomeMae = nomeMae;
-    }
-
-    public void setDataMatricula(String dataMatricula) {
-        this.dataMatricula = dataMatricula;
-    }
-
-    public void setNomeEscola(String nomeEscola) {
-        this.nomeEscola = nomeEscola;
-    }
-
-    public void setSerieMatriculado(String serieMatriculado) {
-        this.serieMatriculado = serieMatriculado;
-    }
-
-    // GETTERS -----------------------------------
-
-    public String getNome() {
-        return nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public String getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public String getRegistroGeral() {
-        return registroGeral;
-    }
-
-    public String getNumeroCpf() {
-        return numeroCpf;
-    }
-
-    public String getNomeMae() {
-        return nomeMae;
-    }
-
-    public String getDataMatricula() {
-        return dataMatricula;
-    }
-
-    public String getNomeEscola() {
-        return nomeEscola;
-    }
-
-    public String getSerieMatriculado() {
-        return serieMatriculado;
-    }
-
-    // METODOS -----------------------------------
 
     @Override
     public String toString() {
@@ -120,7 +30,7 @@ public class Aluno {
         for (Disciplina disciplina : disciplinas) {
             somaNotas += disciplina.getNota();
         }
-        return somaNotas/ disciplinas.size();
+        return somaNotas / disciplinas.size();
 
     }
 
@@ -134,15 +44,15 @@ public class Aluno {
         }
     }
 
-    public String getAlunoAprovado2(){
+    public String getAlunoAprovado2() {
         double media = this.getMediaNota();
-        if (media >= 5){
-            if (media >=7){
+        if (media >= 5) {
+            if (media >= 7) {
                 return "Aluno aprovado!";
-            }else {
+            } else {
                 return "Aluno em recuperação";
             }
-        }else {
+        } else {
             return "Aluno reprovado!";
         }
     }
---
+++ JavaDevFullStack/src/cursojava.classes/Pessoa.java
@@ -0,0 +1,86 @@
+//Iniciando a prática e teórica de HERANÇA POO
+
+public class Pessoa {
+    protected String nome;
+    protected int idade;
+    protected String dataNascimento;
+    protected String registroGeral;
+    protected String numeroCpf;
+    protected String nomeMae;
+    protected String dataMatricula;
+    protected String nomeEscola;
+    protected String serieMatriculado;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getRegistroGeral() {
+        return registroGeral;
+    }
+
+    public void setRegistroGeral(String registroGeral) {
+        this.registroGeral = registroGeral;
+    }
+
+    public String getNumeroCpf() {
+        return numeroCpf;
+    }
+
+    public void setNumeroCpf(String numeroCpf) {
+        this.numeroCpf = numeroCpf;
+    }
+
+    public String getNomeMae() {
+        return nomeMae;
+    }
+
+    public void setNomeMae(String nomeMae) {
+        this.nomeMae = nomeMae;
+    }
+
+    public String getDataMatricula() {
+        return dataMatricula;
+    }
+
+    public void setDataMatricula(String dataMatricula) {
+        this.dataMatricula = dataMatricula;
+    }
+
+    public String getNomeEscola() {
+        return nomeEscola;
+    }
+
+    public void setNomeEscola(String nomeEscola) {
+        this.nomeEscola = nomeEscola;
+    }
+
+    public String getSerieMatriculado() {
+        return serieMatriculado;
+    }
+
+    public void setSerieMatriculado(String serieMatriculado) {
+        this.serieMatriculado = serieMatriculado;
+    }
+
+}
