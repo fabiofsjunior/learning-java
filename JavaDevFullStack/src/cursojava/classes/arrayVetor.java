3917f5a08bd2864e6271e081bbab60bd7e881c75
Fábio F S Jr <59260204+fabiofsjunior@users.noreply.github.com>
2023-01-16 22:02:58 -0300

    new - trabalhando de forma dinâmica com o array.

--- JavaDevFullStack/src/cursojava/classes/arrayVetor.java
+++ JavaDevFullStack/src/cursojava/classes/arrayVetor.java
@@ -1,22 +1,31 @@
 package cursojava.classes;
 
+import javax.swing.JOptionPane;
+
 public class arrayVetor {
-    
+
     public static void main(String[] args) {
 
-        //o Array sempre é necessário definir a quantidade de posições.
-        double[] notas = new double[4];
+        // o Array sempre é necessário definir a quantidade de posições.
 
-        notas[0] = 1.5;
-        notas[1] = 2.1;
-        notas[2] = 3.2;
-        notas[3] = 4.3;
 
-        for (int i = 0; i < 5; i++){
-            System.out.println("Nota na posição: " + i + " é igual a: " + notas[i]);
+
+        //Solicita a quantidade de posições de forma dinâmica.
+        String qtdPosicoes = JOptionPane.showInputDialog("Qual será o tamanho do array?");
+
+        //Converte a quantidade capturada em inteiros para que possa ser utilizado no FOR
+        double[] notas = new double[Integer.parseInt(qtdPosicoes)];
+
+        //Solicitando de forma dinâmica os valores de cada item do Array.
+        for (int pos = 0; pos < notas.length; pos++) {
+
+            notas[pos] = Double.parseDouble(JOptionPane.showInputDialog("Informe a nota para a posição:"));
+
         }
-
-      
+        //Imprimindo no console de foram dinâmica os valores do array.
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println("Nota na posição " + i + " é igual a: " + notas[i]);
+        }
 
     }
 }
